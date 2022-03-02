315f20e64b319fcb1cf27cb566bc0a5650699712
Vikord <98135164+Vikord@users.noreply.github.com>
2022-03-02 13:41:16 -0500

    Initial commit

---
+++ src/TP2/TP2test.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package autobus;
+
+;
+
+/**
+ *
+ * @author thein
+ */
+public class TP2test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
